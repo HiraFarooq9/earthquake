eebf1bf0844d5f9cfb3df1aecb1728aeedd06b2c
HiraFarooq9 <hf99@duke.edu>
2026-03-21 17:12:23 -0400

    fix folder creation

--- run-all.R
+++ run-all.R
@@ -3,3 +3,5 @@ source("R/01-import.R")
 source("R/02-clean.R")
 quarto::quarto_render("report.qmd")
 
+dir.create("data/raw", recursive = TRUE, showWarnings = FALSE)
+dir.create("data/clean", recursive = TRUE, showWarnings = FALSE)
