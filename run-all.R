4f497a36b5cd9065a27f8e405a2af31b1771a518
HiraFarooq9 <hf99@duke.edu>
2026-03-22 13:01:18 -0400

    fix dir.create order

--- run-all.R
+++ run-all.R
@@ -1,7 +1,6 @@
+dir.create("data/raw", recursive = TRUE, showWarnings = FALSE)
+dir.create("data/clean", recursive = TRUE, showWarnings = FALSE)
 
 source("R/01-import.R")
 source("R/02-clean.R")
 quarto::quarto_render("report.qmd")
-
-dir.create("data/raw", recursive = TRUE, showWarnings = FALSE)
-dir.create("data/clean", recursive = TRUE, showWarnings = FALSE)
